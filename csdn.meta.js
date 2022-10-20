8ba88fd8e2153964a24877899c38904aa68936ff
ilyydy <ilyydy@gmail.com>
2022-10-20 16:51:33 +0000

    Deploying to csdn from  @ d1f3d6a223d7139326f82a0a6c2859f9cc7be34f 🚀

--- csdn.meta.js
+++ csdn.meta.js
@@ -2,7 +2,7 @@
 // @name               csdn 页面优化处理
 // @name:zh            csdn 页面优化处理
 // @namespace          https://github.com/ilyydy/tampermonkey-script
-// @version            0.0.1
+// @version            0.0.2
 // @author             ilyydy
 // @description        解除页面对复制的限制
 // @description:zh     解除页面对复制的限制
@@ -10,7 +10,7 @@
 // @license            MIT
 // @icon               https://g.csdnimg.cn/static/logo/favicon32.ico
 // @supportURL         https://github.com/ilyydy/tampermonkey-script/issues
-// @downloadURL        https://github.com/ilyydy/tampermonkey-script/blob/csdn/csdn.user.js
-// @updateURL          https://github.com/ilyydy/tampermonkey-script/blob/csdn/csdn.meta.js
+// @downloadURL        https://raw.githubusercontent.com/ilyydy/tampermonkey-script/csdn/csdn.user.js
+// @updateURL          https://raw.githubusercontent.com/ilyydy/tampermonkey-script/csdn/csdn.meta.js
 // @match              http*://blog.csdn.net/*
 // ==/UserScript==
