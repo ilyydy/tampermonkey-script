152d0b0b34c3d6fe6aced6e204270f8f17a9523d
ilyydy <ilyydy@gmail.com>
2022-10-21 00:48:16 +0800

    fix: new-script 修改 changelog script 中的 TODO

--- dev.sh
+++ dev.sh
@@ -66,7 +66,7 @@ new-script() {
     echo "script dir $name already exists."
   else
     cp -r "$PACKAGES_DIR"/template "$PACKAGES_DIR"/"$name" &&
-      sed -i "0,/TODO/ s/TODO/$name/" "$PACKAGES_DIR"/"$name"/package.json &&
+      sed -i "s/TODO/$name/" "$PACKAGES_DIR"/"$name"/package.json &&
       sed -i "0,/TODO/ s/TODO/$name/" "$PACKAGES_DIR"/"$name"/README.md &&
       pnpm -F "$name" i &&
       echo "create script dir $name done"
