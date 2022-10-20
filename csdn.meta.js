a1b372f5f74760ef9e445087d4c97ed217d7c74b
ilyydy <ilyydy@gmail.com>
2022-10-20 16:32:32 +0000

    Deploying to csdn from  @ 45a74318d7508f8b3b3e4978b1362d90bcc08f99 🚀

---
+++ csdn.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name               csdn 页面优化处理
+// @name:zh            csdn 页面优化处理
+// @namespace          https://github.com/ilyydy/tampermonkey-script
+// @version            0.0.1
+// @author             ilyydy
+// @description        解除页面对复制的限制
+// @description:zh     解除页面对复制的限制
+// @description:zh-CN  解除页面对复制的限制
+// @license            MIT
+// @icon               https://g.csdnimg.cn/static/logo/favicon32.ico
+// @supportURL         https://github.com/ilyydy/tampermonkey-script/issues
+// @downloadURL        https://github.com/ilyydy/tampermonkey-script/blob/csdn/csdn.user.js
+// @updateURL          https://github.com/ilyydy/tampermonkey-script/blob/csdn/csdn.meta.js
+// @match              http*://blog.csdn.net/*
+// ==/UserScript==
---
+++ csdn.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name               csdn 页面优化处理
+// @name:zh            csdn 页面优化处理
+// @namespace          https://github.com/ilyydy/tampermonkey-script
+// @version            0.0.1
+// @author             ilyydy
+// @description        解除页面对复制的限制
+// @description:zh     解除页面对复制的限制
+// @description:zh-CN  解除页面对复制的限制
+// @license            MIT
+// @icon               https://g.csdnimg.cn/static/logo/favicon32.ico
+// @supportURL         https://github.com/ilyydy/tampermonkey-script/issues
+// @downloadURL        https://github.com/ilyydy/tampermonkey-script/blob/csdn/csdn.user.js
+// @updateURL          https://github.com/ilyydy/tampermonkey-script/blob/csdn/csdn.meta.js
+// @match              http*://blog.csdn.net/*
+// ==/UserScript==
+
+(function() {
+  "use strict";
+  function run(doc) {
+    doc.body.contentEditable = "true";
+    for (const element of Array.from(doc.getElementsByTagName("pre"))) {
+      element.style.userSelect = "auto";
+    }
+    for (const element of Array.from(doc.getElementsByTagName("code"))) {
+      element.style.userSelect = "auto";
+      element.removeAttribute("onclick");
+    }
+  }
+  run(document);
+})();
