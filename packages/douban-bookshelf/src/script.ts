ad645c922470baed15c2af2d1a17e6ff7d710aa7
ilyydy <ilyydy@gmail.com>
2022-10-23 23:19:06 +0800

    feat: 书籍详情页临时提交

--- packages/douban-bookshelf/src/script.ts
+++ packages/douban-bookshelf/src/script.ts
@@ -1,3 +1,14 @@
+import { init as subjectInit } from './view/subject/index';
+
 export default function run(doc: Document) {
-  // TODO
+  const map: { [index: string]: (doc: Document) => void } = {
+    ['book.douban.com/subject']: subjectInit,
+  };
+
+  const url = new URL(doc.URL);
+  const initFunc =
+    map[url.host + url.pathname.split('/').slice(0, 2).join('/')];
+  if (initFunc) {
+    initFunc(doc);
+  }
 }
---
+++ packages/douban-bookshelf/src/view/components/ButtonItem.vue
@@ -0,0 +1,9 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+defineProps({});
+</script>
+
+<style scoped></style>
