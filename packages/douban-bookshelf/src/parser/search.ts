05b34f25a68cab4197376f87ef23948cf469efc1
ilyydy <ilyydy@gmail.com>
2022-12-14 15:14:59 +0800

    feat: 优化搜索页面解析

--- packages/douban-bookshelf/src/parser/search.ts
+++ packages/douban-bookshelf/src/parser/search.ts
@@ -2,5 +2,8 @@ export function getBookItemList(searchDoc: Document) {
   const rootEle = searchDoc.querySelector('#wrapper #root');
   if (!rootEle) return [];
 
-  return Array.from(rootEle.querySelectorAll('.item-root'));
+  return Array.from(rootEle.querySelectorAll('.item-root')).filter((i) => {
+    const aEle = i.querySelector('a');
+    return aEle?.href?.startsWith('https://book.douban.com/subject/');
+  });
 }
--- test/douban-bookshelf/parser/search.test.ts
+++ test/douban-bookshelf/parser/search.test.ts
@@ -7,15 +7,24 @@ import { createWindowFromFile } from '../../util';
 import type { Document } from 'happy-dom';
 
 let docQing: Document;
+let docAd: Document;
 
 beforeAll(async () => {
   const windowQing = await createWindowFromFile(
     fileURLToPath(new URL('../html/searchQing.html', import.meta.url))
   );
   docQing = windowQing.document;
+
+  const windowAd = await createWindowFromFile(
+    fileURLToPath(new URL('../html/searchAd.html', import.meta.url))
+  );
+  docAd = windowAd.document;
 });
 
 test('getBookItemList test', () => {
-  const bookItemList = searchParser.getBookItemList(docQing as any);
-  expect(bookItemList.length).toEqual(15);
+  const bookItemListQing = searchParser.getBookItemList(docQing as any);
+  expect(bookItemListQing.length).toEqual(15);
+
+  const bookItemListAd = searchParser.getBookItemList(docAd as any);
+  expect(bookItemListAd.length).toEqual(2);
 });
