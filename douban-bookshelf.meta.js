1b90b2ce77752073f0062d5a83cff92aea724ee0
ilyydy <ilyydy@gmail.com>
2022-12-21 13:32:23 +0000

    Deploying to douban-bookshelf from  @ 14abbcaa74456032b208f4133419d903e009f408 🚀

--- douban-bookshelf.meta.js
+++ douban-bookshelf.meta.js
@@ -2,7 +2,7 @@
 // @name               豆瓣读书书架
 // @name:zh            豆瓣读书书架
 // @namespace          https://github.com/ilyydy/tampermonkey-script
-// @version            0.0.1
+// @version            0.0.2
 // @author             ilyydy
 // @description        无需登录，快速选择书籍加入书架，复制导出书籍信息
 // @description:zh     无需登录，快速选择书籍加入书架，复制导出书籍信息
@@ -15,7 +15,10 @@
 // @match              http*://*book.douban.com/*
 // @match              http*://search.douban.com/book/*
 // @require            https://cdn.jsdelivr.net/npm/vue@3.2.45/dist/vue.global.prod.js
-// @require            https://cdn.sheetjs.com/xlsx-0.19.0/package/dist/xlsx.full.min.js
+// @require            data:application/javascript,window.Vue%3DVue%3B
+// @require            https://cdn.jsdelivr.net/npm/element-plus@2.2.19/dist/index.full.min.js
+// @require            https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js
+// @require            https://cdn.jsdelivr.net/npm/csv-stringify@6.2.3/dist/iife/sync.js
 // @resource           element-plus/dist/index.css  https://cdn.jsdelivr.net/npm/element-plus@2.2.19/dist/index.css
 // @connect            book.douban.com
 // @grant              GM_getResourceText
