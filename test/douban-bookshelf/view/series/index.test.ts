1ad999c991ad4c739907d91348074b30bd2caeee
ilyydy <ilyydy@gmail.com>
2023-02-05 23:34:48 +0800

    test: 完善测试

--- test/douban-bookshelf/view/series/index.test.ts
+++ test/douban-bookshelf/view/series/index.test.ts
@@ -1,4 +1,5 @@
 import { beforeAll, beforeEach, assert, expect, test, vi } from 'vitest';
+import { Window } from 'happy-dom';
 
 import { createWindowFromFileAndUrl, DOU_DAN_PAGE } from '../../../util';
 import * as douban from '../../../../packages/douban-bookshelf/src/common/douban';
@@ -35,4 +36,19 @@ test('useInitBtns test', async () => {
     docNabokov.getElementsByClassName(douban.IDENTIFY_CLASS)
   );
   expect(aElementsAgain.length).toBe(bookItemListNabokov.length * 2);
+});
+
+test('useInitBtns exclude urls test', async () => {
+  const excludeUrls = [
+    'https://book.douban.com/series/add?series_id=927',
+    'https://book.douban.com/series/remove?series_id=927',
+    'https://book.douban.com/series/error?series_id=927',
+  ];
+
+  for (const i of excludeUrls) {
+    const w = new Window();
+    w.happyDOM.setURL(i);
+    const r = useInitBtns(w.document as any);
+    expect(r).toBe(false);
+  }
 });
--- test/douban-bookshelf/view/subject/index.test.ts
+++ test/douban-bookshelf/view/subject/index.test.ts
@@ -1,4 +1,5 @@
 import { beforeAll, beforeEach, assert, expect, test, vi } from 'vitest';
+import { Window } from 'happy-dom';
 
 import { createWindowFromFileAndUrl, DOU_DAN_PAGE } from '../../../util';
 import * as douban from '../../../../packages/douban-bookshelf/src/common/douban';
@@ -33,4 +34,24 @@ test('useInitBtns test', async () => {
     docAlice.getElementsByClassName(douban.IDENTIFY_CLASS)
   );
   expect(aElementsAgain.length).toBe(2);
+});
+
+test('useInitBtns exclude urls test', async () => {
+  const excludeUrls = [
+    'https://book.douban.com/subject/36110302/comments/',
+    'https://book.douban.com/subject/36110302/new_review',
+    'https://book.douban.com/subject/36110302/blockquotes',
+    'https://book.douban.com/subject/36110302/reviews',
+    'https://book.douban.com/subject/36110302/doulists',
+    'https://book.douban.com/subject/36110302/discussion/create',
+    'https://book.douban.com/subject/36110302/offers?new_offer=1',
+    'https://book.douban.com/subject/36110302/annotation',
+  ];
+
+  for (const i of excludeUrls) {
+    const w = new Window();
+    w.happyDOM.setURL(i);
+    const r = useInitBtns(w.document as any);
+    expect(r).toBe(undefined);
+  }
 });
